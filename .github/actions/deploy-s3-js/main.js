0caa6efb49fea299d8823ccb45321ba73d3336ac
baoha7204 <baoha3604@gmail.com>
2024-07-28 21:21:28 +0700

    [BaoHMQ] docker #1

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,21 @@
 const core = import("@actions/core");
+const exec = import("@actions/exec");
 
 function run() {
-  core.notice("Hello!");
+  // Get inputs
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const dist = core.getInput("dist-folder", { required: true });
+
+  // Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${dist} ${s3Uri} --region ${bucketRegion}`);
+
+  // Set outputs
+  core.setOutput(
+    "website-url",
+    `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  );
 }
 
 run();
